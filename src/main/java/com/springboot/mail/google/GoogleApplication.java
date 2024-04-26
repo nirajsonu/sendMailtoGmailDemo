5dbf1a0f755d204d6067c380cbb82fb99e02c96f
NEERAJ KUMAR KESHRI <38843693+nirajsonu@users.noreply.github.com>
2024-04-26 16:15:02 +0530

    2nd commit

---
+++ src/main/java/com/springboot/mail/google/GoogleApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.mail.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleApplication.class, args);
+	}
+
+}
